2944fc695e3c019dcfec8fe0888fc43a900c6771
heruoxin <heruoxin@gmail.com>
2014-12-10 19:57:24 +0800

    notification

--- app/src/main/java/com/catchingnow/clippingnow/CBWatcherService.java
+++ app/src/main/java/com/catchingnow/clippingnow/CBWatcherService.java
@@ -1,5 +1,8 @@
 package com.catchingnow.clippingnow;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ClipData;
 import android.content.ClipDescription;
@@ -8,6 +11,8 @@ import android.content.ClipboardManager.OnPrimaryClipChangedListener;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+
+import java.util.List;
 
 public class CBWatcherService extends Service {
 
@@ -40,8 +45,37 @@ public class CBWatcherService extends Service {
             ClipData cd = cb.getPrimaryClip();
             if (cd.getDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
                 String clip = cd.getItemAt(0).getText().toString();
-                Log.v("clipclip", clip);
+                Log.v(tag, clip);
+                showNotification(clip);
             }
         }
     }
+
+    public List<String> getThreeClips () {
+        return null;
+    }
+
+    public void showNotification(String s) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+// build notification
+// the addAction re-use the same intent to keep the example short
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle(s) //title
+                .setContentText("Subject")
+                .setSmallIcon(R.drawable.ic_drawer)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.ic_drawer, "Call", pIntent)
+                .addAction(R.drawable.ic_drawer, "More", pIntent)
+                .addAction(R.drawable.ic_drawer, "And more", pIntent)
+                .build();
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, n);
+    }
+
 }
