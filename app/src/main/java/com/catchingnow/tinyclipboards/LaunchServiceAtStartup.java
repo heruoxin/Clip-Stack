def56a95ed8cd2efc02a29d367123a9ab10c187b
heruoxin <heruoxin@gmail.com>
2015-01-19 14:23:09 +0800

    startup bug fix

--- app/src/main/java/com/catchingnow/tinyclipboards/LaunchServiceAtStartup.java
+++ app/src/main/java/com/catchingnow/tinyclipboards/LaunchServiceAtStartup.java
@@ -6,12 +6,12 @@ import android.content.Context;
 import android.content.Intent;
 
 public class LaunchServiceAtStartup extends BroadcastReceiver {
-    public LaunchServiceAtStartup() {
-    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent i = new Intent(context, CBWatcherService.class);
-        context.startService(i);
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent i = new Intent(context, CBWatcherService.class);
+            context.startService(i);
+        }
     }
 }
