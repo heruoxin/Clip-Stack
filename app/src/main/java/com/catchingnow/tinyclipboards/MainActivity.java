cef2723b4f862f1bf5c040a6207b894fbb9f6b8b
heruoxin <heruoxin@gmail.com>
2015-01-23 21:41:38 +0800

    add search button and change color

--- app/src/main/java/com/catchingnow/tinyclipboards/MainActivity.java
+++ app/src/main/java/com/catchingnow/tinyclipboards/MainActivity.java
@@ -33,7 +33,10 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+        if (id == R.id.action_search) {
             return true;
         }
 
