3329d34702eae8265cadb6a6e232f8a97de144d3
heruoxin <heruoxin@gmail.com>
2015-01-24 19:58:26 +0800

    add ClipObject

---
+++ app/src/main/java/com/catchingnow/tinyclipboards/ClipObject.java
@@ -0,0 +1,15 @@
+package com.catchingnow.tinyclipboards;
+
+import java.util.Date;
+
+/**
+ * Created by heruoxin on 15/1/24.
+ */
+public class ClipObject {
+    protected String text;
+    protected Date date;
+    public ClipObject(String text, Date date) {
+        this.text = text;
+        this.date = date;
+    }
+}
