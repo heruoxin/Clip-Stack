18ff1ea954a146b3f6f6a94d3e07f1e41a201e9c
heruoxin <heruoxin@gmail.com>
2015-01-28 20:38:48 +0800

    mv StorageHelper

--- app/src/main/java/com/catchingnow/tinyclipboards/Storage.java
+++ app/src/main/java/com/catchingnow/tinyclipboards/Storage.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -140,10 +141,36 @@ public class Storage {
         return true;
     }
 
+    public class StorageHelper extends SQLiteOpenHelper {
+        private final static String PACKAGE_NAME = "com.catchingnow.tinyclipboards";
+        private static final int DATABASE_VERSION = 2;
+        private static final String DATABASE_NAME = "clippingnow.db";
+        private static final String TABLE_NAME = "cliphistory";
+        private static final String CLIP_STRING = "history";
+        private static final String CLIP_DATE = "date";
+        private static final String TABLE_CREATE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        CLIP_DATE + " TIMESTAMP, " +
+                        CLIP_STRING + " TEXT);";
+        public StorageHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(TABLE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.v(PACKAGE_NAME, "SQL updated from" + oldVersion + "to" + newVersion);
+        }
+    }
+
 //    public void printClips(int n) {
 //        for (int i=0; i<n; i++){
 //            String s = getClipHistory(n);
 //            Log.v("printClips", s);
 //        }
 //    }
+
 }
