856806f53c312df5bca374570e92a071d603ecd7
heruoxin <heruoxin@gmail.com>
2015-01-29 21:15:34 +0800

    add save, cancel and up button for editor

--- app/src/main/java/com/catchingnow/tinyclipboards/ActivityEditor.java
+++ app/src/main/java/com/catchingnow/tinyclipboards/ActivityEditor.java
@@ -1,27 +1,44 @@
 package com.catchingnow.tinyclipboards;
 
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class ActivityEditor extends ActionBarActivity {
+
+    private String oldText;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(Intent.EXTRA_TEXT);
+        oldText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        editText = (EditText) findViewById(R.id.edit_text);
+        editText.setText(oldText);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        editText.requestFocus();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_editor, menu);
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
         return true;
     }
 
@@ -31,12 +48,54 @@ public class ActivityEditor extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case (R.id.action_share):
+                shareText();
+                break;
+            case (R.id.action_save):
+                saveText();
+                break;
+            case (R.id.action_cancel):
+                finish();
+                break;
+            case (android.R.id.home):
+                finish();
+                break;
+            default:
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    private void shareText() {
+        String text = editText.getText().toString();
+        Intent openIntent = new Intent(this, StringActionIntentService.class);
+        openIntent.putExtra(ClipCardAdapter.CLIPBOARD_STRING, text);
+        openIntent.putExtra(ClipCardAdapter.CLIPBOARD_ACTION, StringActionIntentService.ACTION_SHARE);
+        startService(openIntent);
+    }
+
+    private void saveText() {
+        String newText = editText.getText().toString();
+        if (oldText.equals(newText)) {
+            Toast.makeText(this,
+                    "Not changed.",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        Storage db = new Storage(this);
+        db.deleteClipHistory(oldText);
+        db.close();
+        if (newText != null && !"".equals(newText)) {
+            ClipboardManager cb = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            cb.setText(newText);
+        } else {
+            Toast.makeText(this,
+                    getString(R.string.toast_deleted),
+                    Toast.LENGTH_SHORT).show();
+        }
+        finish();
+    }
+
 }
