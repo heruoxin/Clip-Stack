a063dbd5de37bd2f0a707cb64408741c4e65b579
heruoxin <heruoxin@gmail.com>
2015-02-04 10:47:35 +0800

    small fix

--- app/src/main/java/com/catchingnow/tinyclipboardmanager/ActivityMain.java
+++ app/src/main/java/com/catchingnow/tinyclipboardmanager/ActivityMain.java
@@ -294,7 +294,7 @@ public class ActivityMain extends ActionBarActivity {
                             .putExtra(StringActionIntentService.CLIPBOARD_ACTION, actionCode);
                     context.startService(openIntent);
                     refreshMainView();
-                    return true;
+                    return false;
                 }
             });
         }
