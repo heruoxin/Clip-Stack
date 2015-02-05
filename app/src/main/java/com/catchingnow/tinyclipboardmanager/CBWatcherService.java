64ba91ef1dea27c8d62122c2ef850a4db2a4695f
heruoxin <heruoxin@gmail.com>
2015-02-05 23:49:09 +0800

    small fix

--- app/src/main/java/com/catchingnow/tinyclipboardmanager/CBWatcherService.java
+++ app/src/main/java/com/catchingnow/tinyclipboardmanager/CBWatcherService.java
@@ -171,7 +171,7 @@ public class CBWatcherService extends Service {
                 );
 
         SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean pinOnTop = preference.getBoolean(ActivitySetting.NOTIFICATION_ALWAYS_SHOW, true);
+        boolean pinOnTop = preference.getBoolean(ActivitySetting.NOTIFICATION_ALWAYS_SHOW, false);
 
         Notification.Builder preBuildNotification = new Notification.Builder(this)
                 .setContentTitle(getString(R.string.clip_notification_title) + thisClipText.get(0).trim()) //title
@@ -219,7 +219,7 @@ public class CBWatcherService extends Service {
         }
 
         SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean pinOnTop = preference.getBoolean(ActivitySetting.NOTIFICATION_ALWAYS_SHOW, true);
+        boolean pinOnTop = preference.getBoolean(ActivitySetting.NOTIFICATION_ALWAYS_SHOW, false);
 
         Intent resultIntent = new Intent(this, ActivityMain.class);
         PendingIntent resultPendingIntent =
