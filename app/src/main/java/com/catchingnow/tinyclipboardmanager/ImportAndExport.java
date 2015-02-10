7363368f7cad9686ed82ec62ccef152d851494e3
heruoxin <heruoxin@gmail.com>
2015-02-10 20:59:46 +0800

    export notification

--- app/src/main/java/com/catchingnow/tinyclipboardmanager/ImportAndExport.java
+++ app/src/main/java/com/catchingnow/tinyclipboardmanager/ImportAndExport.java
@@ -1,10 +1,17 @@
 package com.catchingnow.tinyclipboardmanager;
 
 import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -20,11 +27,6 @@ import java.util.List;
  */
 public class ImportAndExport {
     public final static String PACKAGE_NAME = "com.catchingnow.tinyclipboardmanager";
-    private Context context;
-
-    public ImportAndExport(Context context) {
-        this.context = context;
-    }
 
     private static boolean isExternalStorageReadable() {
         String state = Environment.getExternalStorageState();
@@ -41,8 +43,12 @@ public class ImportAndExport {
     private static File getBackupStorageDir(Context context, Date backupDate) {
         // Get the directory for the user's public pictures directory.
         File file = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DOCUMENTS), context.getString(R.string.backup_file_name)+backupDate+".txt");
+                Environment.DIRECTORY_DOCUMENTS), context.getString(R.string.backup_file_name) + backupDate + ".txt");
         return file;
+    }
+
+    public static void exportDialog(Context context) {
+
     }
 
     public static boolean makeExport(Context context, Date startDate, Date endDate, boolean reverse) {
@@ -55,7 +61,7 @@ public class ImportAndExport {
         }
         List<String> backupStringList = new ArrayList<>();
         backupStringList.add(context.getString(R.string.backup_file_name));
-        for (ClipObject clipObject: clipObjects) { //delete out of date clips
+        for (ClipObject clipObject : clipObjects) { //delete out of date clips
             Date clipObjectDate = clipObject.getDate();
             if (clipObjectDate.after(startDate) && clipObjectDate.before(endDate)) {
                 backupStringList.add("\n" + clipObjectDate.toString() + "\n" + clipObject.getText());
@@ -68,19 +74,39 @@ public class ImportAndExport {
                 backupFile.createNewFile();
             }
             BufferedWriter writer = new BufferedWriter(new FileWriter(backupFile, true /*append*/));
-            for (String backupString: backupStringList) {
+            for (String backupString : backupStringList) {
                 writer.write(backupString);
             }
             writer.close();
+            makeNotification(context, backupFile, true);
             return true;
         } catch (IOException e) {
             Log.e(PACKAGE_NAME, "Backup error: \n" + e.toString());
+            makeNotification(context, backupFile, false);
             return false;
         }
 
     }
 
-    private void makeNotification() {
-
+    private static void makeNotification(Context context, File file, boolean success) {
+        if (success) {
+            Toast.makeText(
+                    context,
+                    context.getString(R.string.export_success) + "\n" + file.getAbsolutePath(),
+                    Toast.LENGTH_LONG
+            ).show();
+            Intent openIntent = new Intent()
+                    .setAction(android.content.Intent.ACTION_VIEW)
+                    .setDataAndType(Uri.fromFile(file), MimeTypeMap.getSingleton().getMimeTypeFromExtension("txt"))
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(openIntent);
+        } else {
+            Toast.makeText(
+                    context,
+                    context.getString(R.string.export_failed),
+                    Toast.LENGTH_LONG
+                    ).show();
+        }
     }
 }
+
