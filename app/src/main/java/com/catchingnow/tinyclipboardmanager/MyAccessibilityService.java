b21300915a6e55c1fcbc8c0be0c4a2083579069b
heruoxin <heruoxin@gmail.com>
2015-04-03 17:17:42 +0800

    daemon

--- app/src/main/java/com/catchingnow/tinyclipboardmanager/MyAccessibilityService.java
+++ app/src/main/java/com/catchingnow/tinyclipboardmanager/MyAccessibilityService.java
@@ -2,12 +2,16 @@ package com.catchingnow.tinyclipboardmanager;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.accessibilityservice.AccessibilityServiceInfoCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.AccessibilityDelegateCompat;
 import android.support.v4.view.accessibility.AccessibilityEventCompat;
 import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
@@ -23,9 +27,10 @@ public class MyAccessibilityService extends AccessibilityService {
 
     private Context context;
     private SharedPreferences preferences;
+    private Handler handler;
 
     private boolean isFWRunning = false;
-    private int wakeUpCBServiceCount = 0;
+    private boolean wakedUpCBService = false;
 
     private void startFloatingWindow() {
         if (isFWRunning) return;
@@ -50,7 +55,26 @@ public class MyAccessibilityService extends AccessibilityService {
     @Override
     protected void onServiceConnected() {
         context = this;
+        handler = new Handler();
         preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        BroadcastReceiver restartCBWatcherService = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (wakedUpCBService) return;
+                CBWatcherService.startCBService(context, false);
+                wakedUpCBService = true;
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        wakedUpCBService = false;
+                    }
+                }, 3*1000);
+            }
+        };
+        LocalBroadcastManager.getInstance(context).registerReceiver(
+                restartCBWatcherService,
+                new IntentFilter(CBWatcherService.ON_DESTROY)
+        );
         super.onServiceConnected();
     }
 
@@ -65,13 +89,6 @@ public class MyAccessibilityService extends AccessibilityService {
                 stopFloatingWindow();
                 return;
             }
-        }
-
-        //CB
-        wakeUpCBServiceCount += 1;
-        if (wakeUpCBServiceCount == 80) {
-            wakeUpCBServiceCount = 0;
-            CBWatcherService.startCBService(context, false);
         }
 
         //FW
